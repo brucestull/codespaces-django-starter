2a4d752157eb45a2e436a8b7f2c05904c4ab1c8e
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-02-06 00:36:41 +0000

    Added docstring to 'index' view function, added 'DJANGO_SECRET_KEY' environment variable to 'SECRET_KEY'.

---
+++ config/core/notes/00_commands_and_links.md
@@ -0,0 +1,14 @@
+# Commands and Links
+
+## Questions and Concepts
+
+* How to get the CodeSpaces environment variables?
+  * `printenv`
+* `python manage.py shell` seems to work as expected.
+
+## Commands
+
+* Start the development server:
+  * `python manage.py runserver`
+
+## Links
--- config/core/views.py
+++ config/core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
+    """
+    `index` view function, renders the `index.html` template.
+    """
+    context = {
+            "title": "My own personal Django Example!",
+    }
     return render(
         request,
         "index.html",
-        {
-            "title": "My own pesonal Django Example!",
-        },
+        context,
     )
